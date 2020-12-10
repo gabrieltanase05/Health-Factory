f7146b556678ec2c1d53a88a6f0678cd5996b6d3
Tanase Gabriel <tanasegabriel05@yahoo.com>
2020-12-10 17:49:39 +0200

    The Loader was added

---
+++ src/components/Loader /Loader.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './style/_component.Loader.css';
+
+function Loader() {
+    return (
+        <div className="c-Loader">
+            <div class="c-Loader__ring">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader
